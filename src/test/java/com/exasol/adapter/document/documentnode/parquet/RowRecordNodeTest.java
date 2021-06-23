757333032aa6fd64cbde5d41cf6503926d192e81
Jakob Braun <jakob.braun@posteo.de>
2021-06-23 06:11:10 -0300

    changes from review

--- src/test/java/com/exasol/adapter/document/documentnode/parquet/RowRecordNodeTest.java
+++ src/test/java/com/exasol/adapter/document/documentnode/parquet/RowRecordNodeTest.java
@@ -1,40 +1,51 @@
 package com.exasol.adapter.document.documentnode.parquet;
 
+import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.INT32;
+import static org.apache.parquet.schema.Type.Repetition.REQUIRED;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import java.util.List;
 
+import org.apache.parquet.schema.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.exasol.adapter.document.documentnode.DocumentStringValue;
-import com.exasol.parquetio.data.Row;
+import com.exasol.adapter.document.documentnode.DocumentDecimalValue;
+import com.exasol.parquetio.data.GenericRow;
 
 class RowRecordNodeTest {
+    public static final String COLUMN_NAME = "test";
+    public static final int COLUMN_VALUE = 123;
+    private GenericRow row;
+
+    @BeforeEach
+    void beforeEach() {
+        final Type column = Types.primitive(INT32, REQUIRED).named(COLUMN_NAME);
+        final MessageType schema = new MessageType("message", column);
+        this.row = new GenericRow(schema, List.of(COLUMN_VALUE));
+    }
 
     @Test
     void testGetValue() {
-        final Row row = mock(Row.class);
-        when(row.getValue("test")).thenReturn("value");
-        final DocumentStringValue result = (DocumentStringValue) new RowRecordNode(row).get("test");
-        assertThat(result.getValue(), equalTo("value"));
+        final DocumentDecimalValue result = (DocumentDecimalValue) new RowRecordNode(this.row).get(COLUMN_NAME);
+        assertThat(result.getValue().intValue(), equalTo(COLUMN_VALUE));
     }
 
     @Test
     void testGetKeyValueMap() {
-        final Row row = mock(Row.class);
-        when(row.getFieldNames()).thenReturn(List.of("key"));
-        when(row.getValue("key")).thenReturn("value");
-        final DocumentStringValue result = (DocumentStringValue) new RowRecordNode(row).getKeyValueMap().get("key");
-        assertThat(result.getValue(), equalTo("value"));
+        final DocumentDecimalValue result = (DocumentDecimalValue) new RowRecordNode(this.row).getKeyValueMap()
+                .get(COLUMN_NAME);
+        assertThat(result.getValue().intValue(), equalTo(COLUMN_VALUE));
     }
 
     @Test
     void testHasKey() {
-        final Row row = mock(Row.class);
-        when(row.hasFieldName("test")).thenReturn(true);
-        assertThat(new RowRecordNode(row).hasKey("test"), equalTo(true));
+        assertThat(new RowRecordNode(this.row).hasKey(COLUMN_NAME), equalTo(true));
+    }
+
+    @Test
+    void testNotHasKey() {
+        assertThat(new RowRecordNode(this.row).hasKey("other"), equalTo(false));
     }
 }
