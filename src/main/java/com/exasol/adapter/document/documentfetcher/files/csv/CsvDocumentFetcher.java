c3c08de2cf8b62d19ec69b99b395d085646e824e
Pieterjan Spoelders <pj.spoelders@gmail.com>
2022-05-31 10:05:47 +0200

    wip

--- src/main/java/com/exasol/adapter/document/documentfetcher/files/csv/CsvDocumentFetcher.java
+++ src/main/java/com/exasol/adapter/document/documentfetcher/files/csv/CsvDocumentFetcher.java
@@ -11,6 +11,7 @@ import com.exasol.adapter.document.documentnode.csv.CsvObjectNode;
 import com.exasol.adapter.document.files.FileTypeSpecificDocumentFetcher;
 import com.exasol.adapter.document.iterators.CloseableIterator;
 import com.exasol.adapter.document.iterators.CloseableIteratorWrapper;
+import com.exasol.adapter.document.iterators.TransformingIterator;
 import com.exasol.errorreporting.ExaError;
 import com.opencsv.exceptions.CsvException;
 import de.siegmar.fastcsv.reader.CsvReader;
@@ -23,7 +24,7 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
- * {@link FileTypeSpecificDocumentFetcher} for JSON files.
+ * {@link FileTypeSpecificDocumentFetcher} for CSV files.
  */
 public class CsvDocumentFetcher implements FileTypeSpecificDocumentFetcher {
     private static final long serialVersionUID = 2783593249946168796L;
@@ -37,45 +38,21 @@ public class CsvDocumentFetcher implements FileTypeSpecificDocumentFetcher {
                     .toString());
         }
         final RemoteFile remoteFile = segment.getFile();
-        //try with resources,
-        try (final InputStream inputStream = remoteFile.getContent().getInputStream();
-                final CsvReader csvReader = buildCsvReader(inputStream)) {
             //documentation on readDocuments
             //https://github.com/exasol/virtual-schema-common-document-files/blob/main/doc/user_guide/document_type_plugin_development_guide.md#the-documentfetcher
-            List csvNodesList = new ArrayList<DocumentNode>();
-            for (final Iterator<CsvRow> iterator = csvReader.iterator(); iterator.hasNext();) {
+            //List csvNodesList = new ArrayList<DocumentNode>();
 
-                final CsvRow csvRow = iterator.next();
-                csvNodesList.add( new CsvObjectNode(csvRow) );
-            }
-            return new CloseableIteratorWrapper<DocumentNode>(csvNodesList.iterator());
-        } catch (IOException csvException) {
-            throw new InputDataException(
-                    ExaError.messageBuilder("E-VSDF-1").message("Error in input file {{JSON_FILE}}.")
-                            .parameter("JSON_FILE", remoteFile.getResourceName()).toString(),
-                    csvException);
-        }
-    }
-
-    private static void iterator() {
-        System.out.print("Iterator loop: ");
-        for (final Iterator<CsvRow> iterator = CsvReader.builder()
-                .build("foo,bar\nfoo2,bar2").iterator(); iterator.hasNext();) {
-
-            final CsvRow csvRow = iterator.next();
-            System.out.print(csvRow.getFields());
-            if (iterator.hasNext()) {
-                System.out.print(" || ");
-            } else {
-                System.out.println();
-            }
-        }
+            final InputStream inputStream = remoteFile.getContent().getInputStream();
+            final CsvReader csvReader = buildCsvReader(inputStream);
+            return new  TransformingIterator<>(new CloseableIteratorWrapper<>(csvReader.iterator(),()-> {csvReader.close();inputStream.close();}),
+                    row -> new CsvObjectNode(row));
     }
 
     @Override
     public boolean supportsFileSplitting() {
         return false;
     }
+
     //todo: add support for custom settings
     private CsvReader buildCsvReader(final InputStream inputStream) {
             //todo: can I do a string conversion here?
--- src/test/java/com/exasol/adapter/document/documentfetcher/files/csv/CsvDocumentFetcherTest.java
+++ src/test/java/com/exasol/adapter/document/documentfetcher/files/csv/CsvDocumentFetcherTest.java
@@ -4,6 +4,7 @@ import com.exasol.adapter.document.documentfetcher.files.*;
 import com.exasol.adapter.document.documentfetcher.files.segmentation.FileSegment;
 import com.exasol.adapter.document.documentfetcher.files.segmentation.FileSegmentDescription;
 import com.exasol.adapter.document.documentnode.DocumentNode;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -18,60 +19,23 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CsvDocumentFetcherTest {
-
     @Test
-    void testClosed() {
-        final AssertStreamIsClosedRemoteFileContent spy = new AssertStreamIsClosedRemoteFileContent("{}");
-        final CsvDocumentFetcher csvDocumentFetcher = new CsvDocumentFetcher();
-        csvDocumentFetcher.readDocuments(new FileSegment(new RemoteFile("", 0, spy), ENTIRE_FILE))
-                .forEachRemaining(x -> {
-                    // just run through
-                });
-        assertThat(spy.isStreamClosed(), equalTo(true));
-    }
-    //TODO
-    @Test
-    void testReadDocument() {
-        final RemoteFile remoteFile = getRemoteFileForString("{\"id\": \"book-1\"}");
-        final CsvDocumentFetcher jsonDocumentFetcher = new CsvDocumentFetcher();
+    void testReadDocuments() {
+        final CsvDocumentFetcher documentFetcher = new CsvDocumentFetcher();
+        final RemoteFile remoteFile = new RemoteFile("", 0,
+                new StringRemoteFileContent("book-1\nbook-2"));
         final List<DocumentNode> result = new ArrayList<>();
-        jsonDocumentFetcher.readDocuments(new FileSegment(remoteFile, ENTIRE_FILE)).forEachRemaining(result::add);
-        assertThat(result.size(), equalTo(1));
-    }
-
-    private RemoteFile getRemoteFileForString(final String content) {
-        return new RemoteFile("string source", 0, new StringRemoteFileContent(content));
+        documentFetcher.readDocuments(new FileSegment(remoteFile, ENTIRE_FILE)).forEachRemaining(result::add);
+        assertThat(result.size(), equalTo(2));
     }
 
     @Test
-    void testReadDocumentWithSyntaxError() {
-        final RemoteFile remoteFile = getRemoteFileForString("{\ninvalid syntax\n}");
-        final CsvDocumentFetcher jsonDocumentFetcher = new CsvDocumentFetcher();
-        final FileSegment segment = new FileSegment(remoteFile, ENTIRE_FILE);
-        final InputDataException exception = assertThrows(InputDataException.class,
-                () -> jsonDocumentFetcher.readDocuments(segment));
-        assertThat(exception.getMessage(), equalTo("E-VSDF-1: Error in input file 'string source'."));
-    }
-
-    @ValueSource(strings = { "", " ", "   ", "\n", "\n " })
-    @ParameterizedTest
-    void testReadEmptyDocument(final String emptyDocumentVariant) {
-        final RemoteFile remoteFile = getRemoteFileForString(emptyDocumentVariant);
-        final CsvDocumentFetcher csvDocumentFetcher = new CsvDocumentFetcher();
-        final FileSegment segment = new FileSegment(remoteFile, ENTIRE_FILE);
-        final InputDataException inputDataException = assertThrows(InputDataException.class,
-                () -> csvDocumentFetcher.readDocuments(segment));
-        assertThat(inputDataException.getMessage(), startsWith("E-VSDF-1: Error in input file 'string source'."));
-    }
-
-    @Test
-    void testExceptionForSegmentedFiles() {
-        final RemoteFile remoteFile = getRemoteFileForString("{}");
-        final CsvDocumentFetcher csvDocumentFetcher = new CsvDocumentFetcher();
+    void testExceptionOnSegmentedFile() {
+        final CsvDocumentFetcher documentFetcher = new CsvDocumentFetcher();
+        final RemoteFile remoteFile = new RemoteFile("", 0, new StringRemoteFileContent("{}"));
         final FileSegment segment = new FileSegment(remoteFile, new FileSegmentDescription(2, 0));
-        final IllegalStateException exception = assertThrows(IllegalStateException.class,
-                () -> csvDocumentFetcher.readDocuments(segment));
-        assertThat(exception.getMessage(),
-                startsWith("F-VSDF-17: The CsvDocumentFetcher does not support loading split files."));
+        final IllegalStateException exception = Assert.assertThrows(IllegalStateException.class,
+                () -> documentFetcher.readDocuments(segment));
+        assertThat(exception.getMessage(), startsWith("F-VSDF-17"));
     }
 }
