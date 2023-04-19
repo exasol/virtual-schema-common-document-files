efdcce140fd990ec24616e68de8a79e15e884468
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-04-19 17:22:50 +0200

    Add CSV initial type converters

--- src/main/java/com/exasol/adapter/document/documentnode/csv/CsvValueTypeConverter.java
+++ src/main/java/com/exasol/adapter/document/documentnode/csv/CsvValueTypeConverter.java
@@ -1,25 +1,128 @@
 package com.exasol.adapter.document.documentnode.csv;
 
-import java.util.List;
+import static java.util.stream.Collectors.toList;
+
+import java.util.*;
 
 import com.exasol.adapter.document.documentnode.DocumentNode;
+import com.exasol.adapter.document.documentnode.holder.BooleanHolderNode;
 import com.exasol.adapter.document.documentnode.holder.StringHolderNode;
-import com.exasol.adapter.document.mapping.ColumnMapping;
+import com.exasol.adapter.document.documentpath.ObjectLookupPathSegment;
+import com.exasol.adapter.document.documentpath.PathSegment;
+import com.exasol.adapter.document.mapping.*;
+import com.exasol.errorreporting.ExaError;
 
 class CsvValueTypeConverter {
 
-    private CsvValueTypeConverter() {
+    private final Map<String, ValueConverter> convertersByColumnName;
+    private final Map<Integer, ValueConverter> convertersByColumnIndex;
+
+    private CsvValueTypeConverter(final Map<String, ValueConverter> convertersByColumnName,
+            final Map<Integer, ValueConverter> convertersByColumnIndex) {
+        this.convertersByColumnName = convertersByColumnName;
+        this.convertersByColumnIndex = convertersByColumnIndex;
     }
 
     static CsvValueTypeConverter create(final List<ColumnMapping> csvColumns) {
-        return new CsvValueTypeConverter();
+        final List<ColumnMapping> filterColums = csvColumns.stream()
+                .filter(col -> !(col instanceof SourceReferenceColumnMapping)).collect(toList());
+        final Map<String, ValueConverter> convertersByColumnName = new HashMap<>();
+        final Map<Integer, ValueConverter> convertersByColumnIndex = new HashMap<>();
+        int index = 0;
+        for (final ColumnMapping columnMapping : filterColums) {
+            final ValueConverter converter = createConverter(columnMapping);
+            convertersByColumnIndex.put(index, converter);
+            convertersByColumnName.put(getColumnName(columnMapping), converter);
+            index++;
+        }
+        return new CsvValueTypeConverter(convertersByColumnName, convertersByColumnIndex);
+    }
+
+    private static String getColumnName(final ColumnMapping columnMapping) {
+        if (!(columnMapping instanceof PropertyToColumnMapping)) {
+            throw unsupportedMappingException(columnMapping);
+        }
+        return getColumnNameFromPath((PropertyToColumnMapping) columnMapping);
+    }
+
+    private static IllegalArgumentException unsupportedMappingException(final ColumnMapping columnMapping) {
+        return new IllegalArgumentException(ExaError.messageBuilder("E-VSDF-62")
+                .message("Column mapping of type {{type}} (value: {{mapping}}) is not supported.",
+                        columnMapping.getClass().getName(), columnMapping)
+                .mitigation("Please use only supported column mappings.").toString());
+    }
+
+    private static String getColumnNameFromPath(final PropertyToColumnMapping propertyMapping) {
+        if (propertyMapping.getPathToSourceProperty().getSegments().size() != 1) {
+            throw new IllegalArgumentException(ExaError.messageBuilder("E-VSDF-63")
+                    .message("Path to source {{path}} has {{segment count}} segments.",
+                            propertyMapping.getPathToSourceProperty(), propertyMapping.getPathToSourceProperty().size())
+                    .mitigation("Please specify a path with exactly one segment.").toString());
+        }
+        final PathSegment segment = propertyMapping.getPathToSourceProperty().getSegments().get(0);
+        if (!(segment instanceof ObjectLookupPathSegment)) {
+            throw new IllegalArgumentException(ExaError.messageBuilder("E-VSDF-64")
+                    .message("Segment {{segment}} of path {{path}} has an unsupported type {{segment type}}.", segment,
+                            propertyMapping.getPathToSourceProperty(), segment.getClass().getName())
+                    .mitigation("Please use only supported path segment of type {{supported segment type}}.",
+                            ObjectLookupPathSegment.class.getName())
+                    .toString());
+        }
+        return ((ObjectLookupPathSegment) segment).getLookupKey();
+    }
+
+    private static ValueConverter createConverter(final ColumnMapping columnMapping) {
+        if (columnMapping instanceof PropertyToVarcharColumnMapping) {
+            return StringHolderNode::new;
+        } else if (columnMapping instanceof PropertyToBoolColumnMapping) {
+            return CsvValueTypeConverter::booleanConverter;
+        }
+        return StringHolderNode::new;
+    }
+
+    private static DocumentNode booleanConverter(final String orgValue) {
+        final String value = orgValue.toLowerCase();
+        if (value.equals("true")) {
+            return new BooleanHolderNode(true);
+        }
+        if (value.equals("false")) {
+            return new BooleanHolderNode(false);
+        }
+        throw new IllegalArgumentException(ExaError.messageBuilder("E-VSDF-65")
+                .message("CSV file contains invalid boolean value {{value}}.", orgValue)
+                .mitigation("Please use only values 'true' and 'false' (case insensitive).").toString());
     }
 
     DocumentNode convert(final String value, final int columnIndex) {
-        return new StringHolderNode(value);
+        return findConverter(columnIndex).convert(value);
+    }
+
+    private ValueConverter findConverter(final int columnIndex) {
+        final ValueConverter converter = this.convertersByColumnIndex.get(columnIndex);
+        if (converter == null) {
+            throw new IllegalStateException(ExaError.messageBuilder("E-VSDF-60").message(
+                    "No CSV value converter found for column {{column index}}. Available converters: {{available converters}}.",
+                    columnIndex, this.convertersByColumnIndex).ticketMitigation().toString());
+        }
+        return converter;
     }
 
     DocumentNode convert(final String value, final String columnName) {
-        return new StringHolderNode(value);
+        return findConverter(columnName).convert(value);
+    }
+
+    private ValueConverter findConverter(final String columnName) {
+        final ValueConverter converter = this.convertersByColumnName.get(columnName);
+        if (converter == null) {
+            throw new IllegalStateException(ExaError.messageBuilder("E-VSDF-61").message(
+                    "No CSV value converter found for column {{column name}}. Available converters: {{available converters}}.",
+                    columnName, this.convertersByColumnName).ticketMitigation().toString());
+        }
+        return converter;
+    }
+
+    @FunctionalInterface
+    interface ValueConverter {
+        DocumentNode convert(String value);
     }
 }
