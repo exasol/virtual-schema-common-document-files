bb2bd634ee8469c14996f2ca19f956e52da9ae87
Christoph Pirkl <christoph.pirkl@exasol.com>
2024-06-12 09:24:26 +0200

    Add unit tests for converter

--- src/test/java/com/exasol/adapter/document/documentfetcher/files/ToUpperSnakeCaseConverterTest.java
+++ src/test/java/com/exasol/adapter/document/documentfetcher/files/ToUpperSnakeCaseConverterTest.java
@@ -7,12 +7,16 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class ToUpperSnakeCaseConverterTest {
-    @ParameterizedTest
+    @ParameterizedTest(name = "Name ''{0}'' converted to ''{1}''")
     @CsvSource({ //
+            "'',''", //
+            "' ','_'", //
             "test, TEST", //
             "myTable, MY_TABLE", //
+            "mytable, MYTABLE", //
             "MyTable, MY_TABLE", //
             "myCSV, MY_CSV", //
+            "MyCSV, MY_CSV", //
             "my_table, MY_TABLE", //
             "MY_TABLE, MY_TABLE", //
             "my column, MY_COLUMN" })
