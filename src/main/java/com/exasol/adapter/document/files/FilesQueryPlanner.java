c809657806d7a68f870cb617a86292ada9b57834
mykhailo.skliar <mykhailo.skliar@exasol.com>
2025-06-03 14:54:27 +0200

    - Added logging for decisions leading to empty query plans - extended unit tests, by adding tests for log message components and the log message itself - had to use package visibility to implement assertions on log message and log message components

--- src/main/java/com/exasol/adapter/document/files/FilesQueryPlanner.java
+++ src/main/java/com/exasol/adapter/document/files/FilesQueryPlanner.java
@@ -1,11 +1,15 @@
 package com.exasol.adapter.document.files;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
+import java.util.logging.Logger;
 
 import com.exasol.adapter.document.QueryPlanner;
 import com.exasol.adapter.document.connection.ConnectionPropertiesReader;
 import com.exasol.adapter.document.documentfetcher.DocumentFetcher;
 import com.exasol.adapter.document.documentfetcher.files.FileFinderFactory;
+import com.exasol.adapter.document.documentfetcher.files.FilesDocumentFetcher;
 import com.exasol.adapter.document.queryplan.*;
 import com.exasol.adapter.document.queryplanning.RemoteTableQuery;
 
@@ -14,6 +18,7 @@ import com.exasol.adapter.document.queryplanning.RemoteTableQuery;
  * depending on the file extension of the request.
  */
 public class FilesQueryPlanner implements QueryPlanner {
+    private static final Logger LOGGER = Logger.getLogger(FilesQueryPlanner.class.getName());
     private final FileFinderFactory fileFinderFactory;
     private final ConnectionPropertiesReader connectionInformation;
 
@@ -29,25 +34,92 @@ public class FilesQueryPlanner implements QueryPlanner {
         this.connectionInformation = connectionInformation;
     }
 
+    /**
+     * Creates a {@link QueryPlan} based on the given {@link RemoteTableQuery} and the maximum number
+     * of parallel document fetchers to use.
+     *
+     * <p>This method extracts the source string and splits the selection into source and post filters.
+     * If a contradiction is detected in the source filter, an {@link EmptyQueryPlan} is returned.
+     * If no document fetchers can be created, it also returns an {@link EmptyQueryPlan}.
+     * Otherwise, a {@link FetchQueryPlan} is returned with the appropriate fetchers and post selection.
+     *
+     * @param remoteTableQuery the query that specifies the remote table and selection criteria
+     * @param maxNumberOfParallelFetchers the maximum number of document fetchers to use concurrently
+     * @return a {@link QueryPlan} representing how the query should be executed
+     */
     @Override
     public QueryPlan planQuery(final RemoteTableQuery remoteTableQuery, final int maxNumberOfParallelFetchers) {
         // access the table mapping here
-        final SourceString sourceString = new SourceString(remoteTableQuery.getFromTable().getRemoteName());
-        final FilesSelectionExtractor.Result splitSelection = new FilesSelectionExtractor(sourceString.getFilePattern())
-                .splitSelection(remoteTableQuery.getSelection());
+        final SourceString sourceString = getSourceString(remoteTableQuery);
+        final FilesSelectionExtractor.Result splitSelection = getSplitSelection(sourceString, remoteTableQuery);
         if (splitSelection.getSourceFilter().hasContradiction()) {
+            LOGGER.fine(() -> getSourceFilterContradictionLogMessage(sourceString, remoteTableQuery));
             return new EmptyQueryPlan();
         }
         final List<DocumentFetcher> documentFetchers = getDocumentFetchers(remoteTableQuery,
                 maxNumberOfParallelFetchers, sourceString, splitSelection);
         if (documentFetchers.isEmpty()) {
+            LOGGER.fine(() -> getEmptyDocumentFetchersLogMessage(sourceString, remoteTableQuery));
             return new EmptyQueryPlan();
         } else {
             return new FetchQueryPlan(documentFetchers, splitSelection.getPostSelection());
         }
     }
 
-    private List<DocumentFetcher> getDocumentFetchers(final RemoteTableQuery remoteTableQuery,
+    /**
+     * Extracts the {@link SourceString} from the remote table's name in the given {@link RemoteTableQuery}.
+     *
+     * @param remoteTableQuery the query containing the remote table information
+     * @return a {@link SourceString} derived from the table's remote name
+     */
+    SourceString getSourceString(final RemoteTableQuery remoteTableQuery) {
+        return new SourceString(remoteTableQuery.getFromTable().getRemoteName());
+    }
+
+    /**
+     * Splits the selection in the given {@link RemoteTableQuery} into source and post filters
+     * using the file pattern from the provided {@link SourceString}.
+     *
+     * @param sourceString the source string containing the file pattern
+     * @param remoteTableQuery the query specifying the selection to split
+     * @return a {@link FilesSelectionExtractor.Result} containing source and post selection filters
+     */
+    FilesSelectionExtractor.Result getSplitSelection(final SourceString sourceString, final RemoteTableQuery remoteTableQuery) {
+        return new FilesSelectionExtractor(sourceString.getFilePattern())
+                .splitSelection(remoteTableQuery.getSelection());
+    }
+
+    /**
+     * Generates a log message indicating a contradiction in the source filter for the given query.
+     *
+     * @param sourceString the source string containing the file pattern
+     * @param remoteTableQuery the query whose selection caused the contradiction
+     * @return a formatted log message describing the contradiction and the fallback behavior
+     */
+    String getSourceFilterContradictionLogMessage(final SourceString sourceString, final RemoteTableQuery remoteTableQuery) {
+        return String.format(
+                "Contradiction detected in source filter for file pattern '%s' with selection '%s'. Returning EmptyQueryPlan.",
+                sourceString.getFilePattern(),
+                remoteTableQuery.getSelection()
+        );
+    }
+
+    /**
+     * Generates a log message indicating that no document fetchers were created for the given query.
+     *
+     * @param sourceString the source string containing the file pattern
+     * @param remoteTableQuery the query that did not result in any document fetchers
+     * @return a formatted log message describing the lack of fetchers and the fallback behavior
+     */
+    String getEmptyDocumentFetchersLogMessage(final SourceString sourceString, final RemoteTableQuery remoteTableQuery) {
+        return String.format(
+                "No document fetchers created for file pattern '%s' with selection '%s'. Returning EmptyQueryPlan.",
+                sourceString.getFilePattern(),
+                remoteTableQuery.getSelection()
+        );
+    }
+
+    List<DocumentFetcher> getDocumentFetchers(final RemoteTableQuery remoteTableQuery,
             final int maxNumberOfParallelFetchers, final SourceString sourceString,
             final FilesSelectionExtractor.Result splitSelection) {
         final String additionalConfiguration = remoteTableQuery.getFromTable().getAdditionalConfiguration();
--- src/test/java/com/exasol/adapter/document/files/FilesQueryPlannerTest.java
+++ src/test/java/com/exasol/adapter/document/files/FilesQueryPlannerTest.java
@@ -1,7 +1,7 @@
 package com.exasol.adapter.document.files;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -56,19 +56,51 @@ class FilesQueryPlannerTest {
 
     @Test
     void testBuildDocumentFetcherForEmptyResult() {
+        final RemoteTableQuery remoteTableQuery = getRemoteTableQuery("test-*.json", new NoPredicate());
+        var remoteQuerySelectionToString = remoteTableQuery.getSelection().toString();
+        assertThat(remoteQuerySelectionToString, equalTo("NoPredicate"));
+
         final RemoteFileFinder loader = mockLoaderThatReturnNoFiles();
         final FilesQueryPlanner queryPlanner = mockQueryPlanner(loader);
-        final RemoteTableQuery remoteTableQuery = getRemoteTableQuery("test-*.json", new NoPredicate());
+        final SourceString sourceString = queryPlanner.getSourceString(remoteTableQuery);
+        assertThat(sourceString.getFilePattern(), equalTo("test-*.json"));
+
+        final FilesSelectionExtractor.Result splitSelection = queryPlanner.getSplitSelection(sourceString, remoteTableQuery);
+        assertThat(queryPlanner.getDocumentFetchers(remoteTableQuery, 10, sourceString, splitSelection).isEmpty(),
+                equalTo(true));
+
+        String emptyDocumentFetchersLogMessage = "No document fetchers created for file pattern 'test-*.json' with selection 'NoPredicate'. Returning EmptyQueryPlan.";
+        assertThat(queryPlanner.getEmptyDocumentFetchersLogMessage(sourceString, remoteTableQuery),
+                equalTo(emptyDocumentFetchersLogMessage));
+
         final QueryPlan queryPlan = queryPlanner.planQuery(remoteTableQuery, 10);
         assertThat(queryPlan, instanceOf(EmptyQueryPlan.class));
     }
 
     @Test
     void testContradiction() {
+        var sqlLiteral = new SqlLiteralString("other.json");
+        var sqlLiteralToString = sqlLiteral.toString();
         final RemoteTableQuery remoteTableQuery = getRemoteTableQuery("test-*.json",
                 new ColumnLiteralComparisonPredicate(AbstractComparisonPredicate.Operator.EQUAL,
-                        new SourceReferenceColumnMapping(), new SqlLiteralString("other.json")));
+                        new SourceReferenceColumnMapping(), sqlLiteral));
+
+        var remoteQuerySelectionToString = remoteTableQuery.getSelection().toString();
+        assertThat(remoteQuerySelectionToString,
+                equalTo(String.format("SOURCE_REFERENCE=%s", sqlLiteralToString)));
+
         final var queryPlanner = new FilesQueryPlanner(null, mock(ConnectionPropertiesReader.class));
+        final SourceString sourceString = queryPlanner.getSourceString(remoteTableQuery);
+        assertThat(sourceString.getFilePattern(), equalTo("test-*.json"));
+
+        final FilesSelectionExtractor.Result splitSelection = queryPlanner.getSplitSelection(sourceString, remoteTableQuery);
+        assertThat(splitSelection.getSourceFilter().hasContradiction(), equalTo(true));
+
+        String sourceFilterContradictionLogMessage = String.format(
+                "Contradiction detected in source filter for file pattern 'test-*.json' with selection 'SOURCE_REFERENCE=%s'. Returning EmptyQueryPlan.",
+                sqlLiteralToString);
+        assertThat(queryPlanner.getSourceFilterContradictionLogMessage(sourceString, remoteTableQuery),
+                equalTo(sourceFilterContradictionLogMessage));
         final QueryPlan queryPlan = queryPlanner.planQuery(remoteTableQuery, 10);
         assertThat(queryPlan, instanceOf(EmptyQueryPlan.class));
     }
